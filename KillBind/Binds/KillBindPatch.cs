758d494da134baf13b43a0a2b6b91c08cd199e73
Confusified <60018584+Confusified@users.noreply.github.com>
2023-12-30 01:46:41 +0100

    Update KillBindPatch.cs

--- KillBind/Binds/KillBindPatch.cs
+++ KillBind/Binds/KillBindPatch.cs
@@ -1,6 +1,7 @@
 using GameNetcodeStuff;
 using HarmonyLib;
 using KillBindNS;
+using System;
 using UnityEngine;
 
 namespace KillBind.Binds
@@ -14,7 +15,12 @@ namespace KillBind.Binds
         {
             if (BasePlugin.ModEnabled.Value && BasePlugin.InputActionInstance.ExplodeKey.triggered && !__instance.isPlayerDead && !__instance.isTypingChat && !HUDManager.Instance.typingIndicator.enabled && (!UnityEngine.Object.FindObjectOfType<Terminal>().terminalInUse && !__instance.inTerminalMenu) && __instance == GameNetworkManager.Instance.localPlayerController)
             {
-                __instance.KillPlayer(Vector3.zero, spawnBody: true, CauseOfDeath.Unknown, 1);
+                if (BasePlugin.DeathCause.Value > Enum.GetValues(typeof(CauseOfDeath)).Length) { BasePlugin.DeathCause.Value = 0; }
+
+                int BlowHeadOff = 0;
+                if (BasePlugin.BlowHeadOff.Value) { BlowHeadOff = 1; } else { BlowHeadOff = 0; }
+
+                __instance.KillPlayer(Vector3.zero, spawnBody: true, (CauseOfDeath)BasePlugin.DeathCause.Value, BlowHeadOff);
             }
         }
     }
